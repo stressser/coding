a550f31d78f44693efaf0c534acbaf8999c2a88f
LeStresser <bperkins726@boyslatin.net>
2025-08-25 13:14:33 +0000

    class work

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -7,4 +7,20 @@
 13
 1
 0
+# Boolean - vaules that represent true and false 
+true
+false
+0
+1
 
+0
+1
+
+""
+"abc"
+
+# Variable = a container for data.
+# to create a variable, we first give it a name,
+# then we assign it a vaule (data).
+
+schoolName = "boys latin"

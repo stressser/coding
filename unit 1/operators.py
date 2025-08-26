781d592ec5d307c0d41a8e10cf23bc74470b28b5
LeStresser <bperkins726@boyslatin.net>
2025-08-26 13:30:09 +0000

    Class work

---
+++ unit 1/operators.py
@@ -0,0 +1,6 @@
+# Operators
+# Operators are a construct (built in system) that
+# data types more actions and power.
+
+# Arithmetic family of operators (math)
+# Arithmetic operators are just math operations

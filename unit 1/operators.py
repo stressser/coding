3edc905345e844ea285f168c5a2d10f25415b69b
LeStresser <bperkins726@boyslatin.net>
2025-08-28 13:24:39 +0000

    class work 28

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -4,3 +4,18 @@
 
 # Arithmetic family of operators (math)
 # Arithmetic operators are just math operations
+# The OR operator checks if ONE of the conditions is TRUE.
+# So long as ONE of the conditions is TRUE, it will be TRUE.
+print (2 == 1 or 3 > 2)
+# If BOTH are TRUE the answer is TRUE.
+
+print (not(3 > 1 and "Beau == Beau"))
+# The NOT operator will give the opposite result.
+# The NOT operator flips the result.
+
+alegebraPassed = True
+historyPassed = True
+
+# does this person need credit recovery
+# true means they passed
+print (not(alegebraPassed== TRUE and historyPassed =TRUE))

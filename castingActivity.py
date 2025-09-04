3a2414f8fe2e4a8d3be847ba755efb7607456507
LeStresser <bperkins726@boyslatin.net>
2025-09-04 13:35:12 +0000

    Data casting

---
+++ castingActivity.py
@@ -0,0 +1,20 @@
+# str() - changes any data type into a string.
+# int() - changes any data into a integer. (whole number)
+# float() - changes any data type into a float. (decimal number)
+
+# Problem 1 
+# Name = input("What is your name:")
+# Age = input("What is your age:")
+
+# Problem 2
+Bonus = '200'
+# print(300 + int(Bonus)) # result should be 500
+
+
+# Problem 3
+# HINT: use w3schools to find a data casting function that will change
+# a data type into a boolean
+
+vaule = 'True'
+
+print(True == Vaule) # result should be true
---
+++ unit 1/builtinFunctions.py
@@ -0,0 +1,13 @@
+# built-in- functions - prewritten code that is stored 
+# in the programming language. So long as you know the
+# keyword you can use it's power.
+
+# Float () function - allow us to transforms a whole number 
+# into a decimal number.
+
+transfer = 50
+
+bank = 12.00
+
+print(float(transfer) + bank)
+

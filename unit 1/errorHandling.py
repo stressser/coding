36705e0b3277fe81bd0413e47641056b28f84fc4
LeStresser <bperkins726@boyslatin.net>
2025-09-11 13:00:31 +0000

    Data activity

---
+++ unit 1/errorHandling.py
@@ -0,0 +1,22 @@
+# type error = (data type error), the computer
+# has detected an issue with the data types in your
+# code.
+
+print('The computer lab is in room' + str(105))
+print('100' + 100)
+
+# syntax error = the computer has detected an issue
+# with how you wrote your code. Somewhere you broke
+# the programming language rules and conventions.
+
+books10 = 10
+student = 'ian'
+print(1 + books10)
+
+# arithemetic error = the computer detected 
+# an issue with your math expressions.
+
+print(100/0)
+
+
+# indentation error = 
